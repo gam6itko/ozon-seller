a595ad4fb3bbb8ee8f569b3d8590552ccaa492c6
gambit/ubuntu <gam6itko@gmail.com>
2019-03-20 15:41:48 +0300

    actualize category handlers to v1.1.1 (2019-03-19)

--- src/Service/CategoriesService.php
+++ src/Service/CategoriesService.php
@@ -10,26 +10,37 @@ class CategoriesService extends AbstractService
      * Receive the list of all available item categories.
      * @see http://cb-api.test.ozon.ru/apiref/en/#t-title_get_categories_tree
      * @param int $categoryId
+     * @param string $language [EN, RU]
      * @return array
+     * @throws \Exception
      */
-    public function tree(int $categoryId = null)
+    public function tree(int $categoryId = null, string $language = 'RU')
     {
-        return $this->request('GET', "/v1/categories/tree/{$categoryId}");
+        $query = array_filter([
+            "category_id" => $categoryId,
+            "language"    => strtoupper($language)
+        ]);
+        return $this->request('POST', "/v1/category/tree/{$categoryId}", ['body' => \GuzzleHttp\json_encode($query)]);
     }
 
     /**
      * Receive the attributes list from the product page for a specified category.
      * @see http://cb-api.test.ozon.ru/apiref/en/#t-title_get_categories_attributes
      * @param int $categoryId
+     * @param string $language [EN, RU]
      * @param array $query
      * @return mixed|\Psr\Http\Message\ResponseInterface
      * @throws \Exception
      */
-    public function attributes(int $categoryId, array $query = [])
+    public function attributes(int $categoryId, string $language = 'RU', array $query = [])
     {
         $query = $this->faceControl($query, ['attribute_type']);
         $query = TypeCaster::castArr($query, ['attribute_type' => 'str']);
+        $query = array_merge([
+            "category_id" => $categoryId,
+            "language"    => strtoupper($language)
+        ], $query);
 
-        return $this->request('GET', "/v1/categories/{$categoryId}/attributes", ['query' => $query]);
+        return $this->request('POST', "/v1/category/attribute", ['body' => \GuzzleHttp\json_encode($query)]);
     }
 }
