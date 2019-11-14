aea30d7e0e7fe8d6acb5da9fed5ca454b4033c8c
gam6itko-ubuntu <gam6itko@gmail.com>
2019-11-14 16:14:17 +0300

    constant psr

--- src/Enum/DimensionUnit.php
+++ src/Enum/DimensionUnit.php
@@ -4,7 +4,14 @@ namespace Gam6itko\OzonSeller\Enum;
 
 final class DimensionUnit
 {
+    const MILLIMETERS = 'mm';
+    const CENTIMETRES = 'cm';
+    const INCHES = 'in';
+
+    /** @deprecated use MILLIMETERS */
     const Millimeters = 'mm';
+    /** @deprecated use CENTIMETRES */
     const Centimetres = 'cm';
+    /** @deprecated use INCHES */
     const Inches = 'in';
 }
--- src/Enum/Status.php
+++ src/Enum/Status.php
@@ -4,22 +4,36 @@ namespace Gam6itko\OzonSeller\Enum;
 
 final class Status
 {
+    const AWAITING_APPROVE = 'awaiting_approve';
+    const AWAITING_PACKAGING = 'awaiting_packaging';
+    const AWAITING_DELIVER = 'awaiting_deliver';
+    const DELIVERING = 'delivering';
+    const DELIVERED = 'delivered';
+    const CANCELLED = 'cancelled';
+
+    // todo remove in 0.2
+    /** @deprecated use AWAITING_APPROVE */
     const AwaitingApprove = 'awaiting_approve';
+    /** @deprecated use AWAITING_PACKAGING */
     const AwaitingPackaging = 'awaiting_packaging';
+    /** @deprecated use AWAITING_DELIVER */
     const AwaitingDeliver = 'awaiting_deliver';
+    /** @deprecated use DELIVERING */
     const Delivering = 'delivering';
+    /** @deprecated use DELIVERED */
     const Delivered = 'delivered';
+    /** @deprecated use CANCELLED */
     const Cancelled = 'cancelled';
 
     public static function getList()
     {
         return [
-            self::AwaitingApprove,
-            self::AwaitingPackaging,
-            self::AwaitingDeliver,
-            self::Delivering,
-            self::Delivered,
-            self::Cancelled,
+            self::AWAITING_APPROVE,
+            self::AWAITING_PACKAGING,
+            self::AWAITING_DELIVER,
+            self::DELIVERING,
+            self::DELIVERED,
+            self::CANCELLED,
         ];
     }
 }
