a4995bba0f93ff087552aaacb168184b7ae58166
gam6itko <gam6itko@gmail.com>
2020-06-08 21:45:28 +0300

    declare(strict_types=1);

--- src/Service/V2/Posting/FbsService.php
+++ src/Service/V2/Posting/FbsService.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Gam6itko\OzonSeller\Service\V2\Posting;
 
--- tests/E2E/Service/V2/Posting/FbsServiceTest.php
+++ tests/E2E/Service/V2/Posting/FbsServiceTest.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Gam6itko\OzonSeller\Tests\E2E\Service\V2\Posting;
 
