df0bf4ebb8ad5e031c840da64d6b8ad3cae463e0
gam6itko <gam6itko@gmail.com>
2022-06-08 16:38:09 +0300

    [fix #57] realize `/v1/brand/company-certification/list`

---
+++ src/Service/V1/BrandService.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gam6itko\OzonSeller\Service\V1;
+
+use Gam6itko\OzonSeller\Service\AbstractService;
+use Gam6itko\OzonSeller\TypeCaster;
+use Gam6itko\OzonSeller\Utils\ArrayHelper;
+
+class BrandService extends AbstractService
+{
+    private $path = '/v1/brand';
+
+    /**
+     * Get a list of brands that require a certificate. The response contains a list of brands whose products are added in your seller profile.
+     * The list of brands may change if Ozon receives a requirement from the brand to provide a certificate.
+     *
+     * @see https://docs.ozon.ru/api/seller/en/#operation/BrandAPI_BrandCompanyCertificationList
+     */
+    public function companyCertificationList(array $query = []): array
+    {
+        $pagination = array_merge(
+            ['page' => 1, 'page_size' => 100],
+            ArrayHelper::pick($query, ['page', 'page_size'])
+        );
+
+        return $this->request('POST', "{$this->path}/company-certification/list", $pagination);
+    }
+}
---
+++ tests/Service/V1/BrandServiceTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gam6itko\OzonSeller\Tests\Service\V1;
+
+use Gam6itko\OzonSeller\Service\V1\BrandService;
+use Gam6itko\OzonSeller\Tests\Service\AbstractTestCase;
+
+class BrandServiceTest extends AbstractTestCase
+{
+    protected function getClass(): string
+    {
+        return BrandService::class;
+    }
+
+    public function testCompanyCertificationList(): void
+    {
+        $this->quickTest(
+            'companyCertificationList',
+            [
+                ['page_size' => 50]
+            ],
+            [
+                'POST',
+                '/v1/brand/company-certification/list',
+                '{"page":1,"page_size":50}',
+            ]
+        );
+    }
+}
