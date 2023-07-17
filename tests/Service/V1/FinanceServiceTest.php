e6cb14d1cf7a92c90fe3ffe0f5b33982df972ac9
gam6itko <gam6itko@gmail.com>
2023-07-17 11:00:54 +0300

    fix #70

---
+++ tests/Service/V1/FinanceServiceTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gam6itko\OzonSeller\Tests\Service\V1;
+
+use Gam6itko\OzonSeller\Service\V1\FinanceService;
+use Gam6itko\OzonSeller\Tests\Service\AbstractTestCase;
+
+class FinanceServiceTest extends AbstractTestCase
+{
+    protected function getClass(): string
+    {
+        return FinanceService::class;
+    }
+
+    public function testImportInfo(): void
+    {
+        $this->quickTest(
+            'realization',
+            [
+                ['date' => '2022-02'],
+            ],
+            [
+                'POST',
+                '/v1/finance/realization',
+                '{"date":2022-02}',
+            ]
+        );
+    }
+}
