4ffd00919ddcaada31d7b30e95e0b2cd92486dca
temamich <temamich18@gmail.com>
2023-12-12 19:43:01 +0300

    Добавлены методы 4'ой и 5'ой версии для FbsService

---
+++ src/Service/V4/Posting/FbsService.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gam6itko\OzonSeller\Service\V4\Posting;
+
+use Gam6itko\OzonSeller\Enum\PostingScheme;
+use Gam6itko\OzonSeller\Service\AbstractService;
+use Gam6itko\OzonSeller\Utils\ArrayHelper;
+use Gam6itko\OzonSeller\Utils\WithResolver;
+
+class FbsService extends AbstractService
+{
+    private $path = '/v4/posting/fbs';
+
+    /**
+     * @see https://docs.ozon.ru/api/seller/#operation/PostingAPI_ShipFbsPostingV4
+     *
+     * @return array list of postings IDs
+     */
+    public function ship(array $packages, string $postingNumber, array $options = []): array
+    {
+        foreach ($packages as &$package) {
+            $package = ArrayHelper::pick($package, ['products']);
+        }
+
+        $body = [
+            'packages'       => $packages,
+            'posting_number' => $postingNumber,
+            'with'           => WithResolver::resolve($options, 3, PostingScheme::FBS, __FUNCTION__),
+        ];
+
+        return $this->request('POST', "$this->path/ship", $body);
+    }
+
+    /**
+     * @see https://docs.ozon.ru/api/seller/#operation/PostingAPI_GetProductExemplarStatus
+     */
+    public function getExemplarsStatus($postingNumber): array
+    {
+        $body = [
+            'posting_number' => $postingNumber,
+        ];
+
+        return $this->request('POST', '/v4/fbs/posting/product/exemplar/status', $body);
+    }
+}
