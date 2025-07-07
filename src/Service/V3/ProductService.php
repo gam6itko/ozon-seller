b814f383724f73ec1dd40244d253da471967369f
Rostunov Sergey <sergey@rostunov.com>
2025-07-07 17:20:07 +0300

    Add new ProductService::import method (#102)
    * Add new ProductService::import method
    * Add new ProductService::infoAttributes method

--- src/Service/V3/ProductService.php
+++ src/Service/V3/ProductService.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Gam6itko\OzonSeller\Service\V3;
 
 use Gam6itko\OzonSeller\Enum\SortDirection;
+use Gam6itko\OzonSeller\ProductValidator;
 use Gam6itko\OzonSeller\Service\AbstractService;
 use Gam6itko\OzonSeller\TypeCaster;
 use Gam6itko\OzonSeller\Utils\ArrayHelper;
@@ -66,6 +67,34 @@ class ProductService extends AbstractService
 {
     private $path = '/v3/product';
 
+    /**
+     * This method allows you to create products and update their details.
+     *
+     * @see https://docs.ozon.ru/api/seller/en/#operation/ProductAPI_ImportProductsV3
+     *
+     * @param array $income Single item structure or array of items
+     *
+     * @return array
+     */
+    public function import(array $income, bool $validateBeforeSend = true)
+    {
+        if (!array_key_exists('items', $income)) {
+            $income = $this->ensureCollection($income);
+            $income = ['items' => $income];
+        }
+
+        $income = ArrayHelper::pick($income, ['items']);
+
+        if ($validateBeforeSend) {
+            $pv = new ProductValidator('create', 3);
+            foreach ($income['items'] as &$item) {
+                $item = $pv->validateItem($item);
+            }
+        }
+
+        return $this->request('POST', "{$this->path}/import", $income);
+    }
+    
     public function importStocks(array $filter, ?string $lastId = '', int $limit = 100)
     {
         assert($limit > 0 && $limit <= 1000);
---
+++ src/config/product_validator_v3.php
@@ -0,0 +1,124 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'offer_id' => [
+        'type' => 'string',
+        'requiredCreate' => true,
+    ],
+
+    'description_category_id' => [
+        'type' => 'integer',
+        'requiredCreate' => true,
+    ],
+
+    'type_id' => [
+        'type' => 'integer',
+        'requiredCreate' => false,
+    ],
+
+    'name' => [
+        'type' => 'string',
+        'requiredCreate' => true,
+    ],
+
+    'description' => [
+        'type' => 'string',
+        'requiredCreate' => false,
+    ],
+
+    'attributes' => [
+        'type' => 'array',
+        'requiredCreate' => true,
+    ],
+
+    'complex_attributes' => [
+        'type' => 'array',
+        'requiredCreate' => false,
+    ],
+
+    'barcode' => [
+        'type' => 'string',
+        'requiredCreate' => false,
+    ],
+
+    'width' => [
+        'type' => 'integer',
+        'requiredCreate' => true,
+    ],
+
+    'height' => [
+        'type' => 'integer',
+        'requiredCreate' => true,
+    ],
+
+    'depth' => [
+        'type' => 'integer',
+        'requiredCreate' => true,
+    ],
+
+    'dimension_unit' => [
+        'type' => 'string',
+        'requiredCreate' => true,
+        'options' => ['mm', 'cm', 'in'],
+    ],
+
+    'weight' => [
+        'type' => 'integer',
+        'requiredCreate' => true,
+    ],
+
+    'weight_unit' => [
+        'type' => 'string',
+        'requiredCreate' => true,
+        'options' => ['g', 'kg', 'lb'],
+    ],
+
+    'primary_image' => [
+        'type' => 'string',
+        'requiredCreate' => false,
+    ],
+
+    'images' => [
+        'type' => 'arrayOfString',
+        'requiredCreate' => true,
+    ],
+
+    'images360' => [
+        'type' => 'arrayOfString',
+        'requiredCreate' => false,
+    ],
+
+    'color_image' => [
+        'type' => 'string',
+        'requiredCreate' => false,
+    ],
+
+    'price' => [
+        'type' => 'string',
+        'requiredCreate' => true,
+    ],
+
+    'old_price' => [
+        'type' => 'string',
+        'requiredCreate' => false,
+    ],
+
+    'vat' => [
+        'type' => 'string',
+        'requiredCreate' => true,
+        'options' => ['0', '0.05', '0.07', '0.1', '0.2'],
+    ],
+
+    'currency_code' => [
+        'type' => 'string',
+        'requiredCreate' => true,
+        'options' => ['RUB', 'BYN', 'KZT', 'EUR', 'USD', 'CNY'],
+    ],
+
+    'pdf_list' => [
+        'type' => 'arrayOfString',
+        'requiredCreate' => false,
+    ],
+];
