a3c61a9ca544d2e3f5efc80b8bd8bd1b735883fd
exlh <51967666+exlh@users.noreply.github.com>
2025-11-24 12:58:30 +0300

    Add /v1/posting/fbs/cancel-reason (#111) (#112)

--- src/Service/V1/Posting/FbsService.php
+++ src/Service/V1/Posting/FbsService.php
@@ -6,7 +6,17 @@ namespace Gam6itko\OzonSeller\Service\V1\Posting;
 
 use Gam6itko\OzonSeller\Service\AbstractService;
 
-
+/**
+ * @psalm-type TCancelReasonData = array{
+ *     id: int,
+ *     title: string,
+ *     type_id: string
+ * }
+ * @psalm-type TCancelReasonResponseData = array{
+ *     posting_number: string,
+ *     reasons: TCancelReasonData[]
+ * }
+ */
 class FbsService extends AbstractService
 {
     private $path = '/v1/posting/fbs';
@@ -19,6 +29,27 @@ class FbsService extends AbstractService
         $body = [
             'task_id' => $taskId,
         ];
+
         return $this->request('POST', "{$this->path}/package-label/get", $body);
+    }
+
+    /**
+     * @see https://docs.ozon.ru/api/seller/#operation/PostingAPI_GetPostingFbsCancelReasonV1
+     *
+     * @param string[] $postingNumbers shipment numbers
+     *
+     * @return TCancelReasonResponseData[]
+     */
+    public function cancelReason(array $postingNumbers): array
+    {
+        if (empty($postingNumbers)) {
+            throw new \InvalidArgumentException('Empty posting list');
+        }
+
+        $body = [
+            'related_posting_numbers' => $postingNumbers,
+        ];
+
+        return $this->request('POST', "{$this->path}/cancel-reason", $body);
     }
 }
